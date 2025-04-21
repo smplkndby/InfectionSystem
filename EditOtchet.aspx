31ebc5b49de6a89651d49657cb0fdc44e4b3dabf
my <sabyrovmyrzabek1@gmail.com>
2025-04-21 15:41:29 +0600

    Добавил форму изменения

---
+++ EditOtchet.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EditOtchet.aspx.cs" Inherits="AIS_Infekcii_Web.EditOtchet" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Редактировать отчет</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+</head>
+<body>
+    <form id="form1" runat="server" class="container mt-5">
+        <h2>Редактировать отчёт</h2>
+        <asp:Label runat="server" ID="lblMessage" CssClass="text-danger" />
+        
+        <div class="mb-3">
+            <label>Название болезни:</label>
+            <asp:TextBox ID="txtBolezn" runat="server" CssClass="form-control" />
+        </div>
+        <div class="mb-3">
+            <label>Количество заболевших:</label>
+            <asp:TextBox ID="txtKolvo" runat="server" CssClass="form-control" TextMode="Number" />
+        </div>
+        <div class="mb-3">
+            <label>Дата отчета:</label>
+            <asp:TextBox ID="txtData" runat="server" CssClass="form-control" TextMode="Date" />
+        </div>
+        <div class="mb-3">
+            <label>ID района:</label>
+            <asp:TextBox ID="txtRajonId" runat="server" CssClass="form-control" TextMode="Number" />
+        </div>
+
+        <asp:Button runat="server" ID="btnUpdate" Text="Сохранить изменения" CssClass="btn btn-primary w-100" OnClick="btnUpdate_Click" />
+    </form>
+</body>
+</html>
---
+++ EditOtchet.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AIS_Infekcii_Web
+{
+    public partial class EditOtchet : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["id"] != null)
+                {
+                    int id = int.Parse(Request.QueryString["id"]);
+                    LoadOtchet(id);
+                }
+                else
+                {
+                    lblMessage.Text = "ID не передан в адресной строке.";
+                }
+            }
+        }
+
+        private void LoadOtchet(int id)
+        {
+            string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+            string query = "SELECT * FROM Otchet WHERE OtchetId = @id";
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@id", id);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    txtBolezn.Text = reader["NazvanieBolezni"].ToString();
+                    txtKolvo.Text = reader["KolichestvoZabolevshih"].ToString();
+                    txtData.Text = Convert.ToDateTime(reader["DataOtcheta"]).ToString("yyyy-MM-dd");
+                    txtRajonId.Text = reader["RajonId"].ToString();
+                }
+                else
+                {
+                    lblMessage.Text = "Запись не найдена.";
+                }
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (Request.QueryString["id"] != null)
+            {
+                int id = int.Parse(Request.QueryString["id"]);
+                string bolezn = txtBolezn.Text;
+                int kolvo = int.Parse(txtKolvo.Text);
+                DateTime data = DateTime.Parse(txtData.Text);
+                int rajonId = int.Parse(txtRajonId.Text);
+
+                string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+                string query = "UPDATE Otchet SET NazvanieBolezni=@Bolezn, KolichestvoZabolevshih=@Kolvo, DataOtcheta=@Data, RajonId=@RajonId WHERE OtchetId=@Id";
+
+                using (SqlConnection conn = new SqlConnection(connStr))
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Bolezn", bolezn);
+                    cmd.Parameters.AddWithValue("@Kolvo", kolvo);
+                    cmd.Parameters.AddWithValue("@Data", data);
+                    cmd.Parameters.AddWithValue("@RajonId", rajonId);
+                    cmd.Parameters.AddWithValue("@Id", id);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+                Response.Redirect("OtchetyClassic.aspx");
+            }
+        }
+    }
+}
