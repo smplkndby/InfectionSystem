7c027d905a6bb7e6ea343ad73f91272fab093a49
my <sabyrovmyrzabek1@gmail.com>
2025-04-21 15:44:05 +0600

    Добавил форму удаления

---
+++ DeleteOtchet.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DeleteOtchet.aspx.cs" Inherits="AIS_Infekcii_Web.DeleteOtchet" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Удалить отчет</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server" class="container mt-5">
+        <h2 class="text-danger">Удалить отчет</h2>
+        <asp:Label runat="server" ID="lblMessage" CssClass="text-danger fw-bold" />
+
+        <div class="alert alert-warning mt-4" role="alert">
+            <p>Вы уверены, что хотите удалить отчёт:</p>
+            <p><strong>Болезнь:</strong> <asp:Label runat="server" ID="lblBolezn" /></p>
+            <p><strong>Дата:</strong> <asp:Label runat="server" ID="lblData" /></p>
+        </div>
+
+        <asp:Button runat="server" ID="btnDelete" Text="Удалить" CssClass="btn btn-danger me-2" OnClick="btnDelete_Click" />
+        <a href="OtchetyClassic.aspx" class="btn btn-secondary">Отмена</a>
+    </form>
+</body>
+</html>
---
+++ DeleteOtchet.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AIS_Infekcii_Web
+{
+    public partial class DeleteOtchet : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack && Request.QueryString["id"] != null)
+            {
+                int id = int.Parse(Request.QueryString["id"]);
+                LoadOtchet(id);
+            }
+        }
+
+        private void LoadOtchet(int id)
+        {
+            string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+            string query = "SELECT NazvanieBolezni, DataOtcheta FROM Otchet WHERE OtchetId = @id";
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@id", id);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    lblBolezn.Text = reader["NazvanieBolezni"].ToString();
+                    lblData.Text = Convert.ToDateTime(reader["DataOtcheta"]).ToShortDateString();
+                }
+                else
+                {
+                    lblMessage.Text = "Запись не найдена.";
+                    btnDelete.Enabled = false;
+                }
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (Request.QueryString["id"] != null)
+            {
+                int id = int.Parse(Request.QueryString["id"]);
+
+                string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+                string query = "DELETE FROM Otchet WHERE OtchetId = @id";
+
+                using (SqlConnection conn = new SqlConnection(connStr))
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+                Response.Redirect("OtchetyClassic.aspx");
+            }
+        }
+    }
+}
